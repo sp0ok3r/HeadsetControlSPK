ce70d9978681d8c11d30c017f69579427dc5cf1a
sp0ok3r <digitalfoxanon@hotmail.com>
2023-08-25 23:15:48 +0100

    Add project files.

---
+++ HeadsetControlSPK/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace HeadsetControlSPK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.forceCheckToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "HeadSetControl SP0OK3R";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.forceCheckToolStripMenuItem,
+            this.reloadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(128, 92);
+            // 
+            // forceCheckToolStripMenuItem
+            // 
+            this.forceCheckToolStripMenuItem.Name = "forceCheckToolStripMenuItem";
+            this.forceCheckToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.forceCheckToolStripMenuItem.Text = "Force Check %";
+            this.forceCheckToolStripMenuItem.Click += new System.EventHandler(this.forceCheckToolStripMenuItem_Click);
+            // 
+            // reloadToolStripMenuItem
+            // 
+            this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
+            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.reloadToolStripMenuItem.Text = "Reload";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 93);
+            this.Enabled = false;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem forceCheckToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ HeadsetControlSPK/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace HeadsetControlSPK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string PROGRAMname = "HeadSetControl SP0OK3R";
+        Process process = new Process();
+
+        System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
+
+        private void HeadsetStatus()
+        {
+            process.StartInfo.FileName = "headsetcontrol-windows.exe";
+            process.StartInfo.Arguments = " -b";
+
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+
+
+            string result = output.Split('\n')[1];
+            string bat = Regex.Match(result, @"\d+").Value.ToString();
+
+            int a = int.Parse(bat);
+            if ((50 <= a && a <= 100))
+            {
+                this.notifyIcon1.Icon = Properties.Resources.ssfull;
+            }
+            else if (20 <= a && a <= 49)
+            {
+                this.notifyIcon1.Icon = Properties.Resources.ssmid;
+            }
+            else
+            {
+                this.notifyIcon1.Icon = Properties.Resources.ssred;
+            }
+            this.notifyIcon1.Text = PROGRAMname + "\n" + bat + " %";
+            process.WaitForExit();
+
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Hide();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HeadsetStatus();
+            Hide();
+            t.Interval = 3600000;
+            t.Tick += new EventHandler(timer_Tick);
+            t.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            HeadsetStatus();
+        }
+
+        private void reloadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void forceCheckToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HeadsetStatus();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
